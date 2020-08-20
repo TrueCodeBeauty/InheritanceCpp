827da5587505e898463169fdb74a76fb5ee3a4d0
Saldina Nurak <saldina.nurak@edu.fit.ba>
2020-08-20 18:41:20 +0200

    Initial commit

---
+++ InheritanceCpp/InheritanceCpp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class YouTubeChannel {
+private:
+	string Name;
+	string OwnerName;
+	int SubscribersCount;
+	list<string> PublishedVideoTitles;
+public:
+	YouTubeChannel(string name, string ownerName) {
+		Name = name;
+		OwnerName = ownerName;
+		SubscribersCount = 0;
+	}
+	void GetInfo() {
+		cout << "Name: " << Name << endl;
+		cout << "OwnerName: " << OwnerName << endl;
+		cout << "SubscribersCount: " << SubscribersCount << endl;
+		cout << "Videos: " << endl;
+		for (string videoTitle : PublishedVideoTitles) {
+			cout << videoTitle << endl;
+		}
+	}
+	void Subscribe() {
+		SubscribersCount++;
+	}
+	void Unsubscribe() {
+		if (SubscribersCount > 0)
+			SubscribersCount--;
+	}
+	void PublishVideo(string title) {
+		PublishedVideoTitles.push_back(title);
+	}
+};
+
+int main()
+{
+
+
+	system("pause>0");
+}
